35037ad3de46f4b85b08249feecc8c07c1ab0b4d
Kyungwoon Na <nakw0410@gmail.com>
2022-01-06 16:02:50 +0900

    feat: 나홀로 메모장 삭제 기능 추가

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import requests
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
+
+client = MongoClient('mongodb://test:test@localhost', 27017)
+# client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
 ## HTML을 주는 부분
@@ -35,16 +37,23 @@ def saving():
     desc = soup.select_one('meta[property="og:description"]')['content']
 
     doc = {
-        'title': title,
-        'image': image,
-        'desc': desc,
-        'url': url_receive,
-        'comment': comment_receive
-
+        'title':title,
+        'image':image,
+        'desc':desc,
+        'url':url_receive,
+        'comment':comment_receive
     }
+
     db.articles.insert_one(doc)
 
-    return jsonify({'msg':'저장이 완료되었습니다.'})
+
+    return jsonify({'msg':'저장이 완료되었습니다!'})
+
+@app.route('/memo/delete', methods=['POST'])
+def delete_movie():
+    title_receive = request.form['title_give']
+    db.articles.delete_one({'title':title_receive})
+    return jsonify({'msg': '삭제 완료!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
